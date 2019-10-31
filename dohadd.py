cd63330011e4aa090e75d42ff8a6fb9ba851a563
Leizuohong <lzh791397845@gmail.com>
2019-10-31 16:29:51 +0800

    123

--- dohadd.py
+++ dohadd.py
@@ -16,13 +16,13 @@ else:
     # 处理所有能量点
     if(sys.argv[1] == '-1'):
         for i in inenergy_list:
-            os.system('rm %s_%s.root' % (inname, str(i)))
-            os.system('hadd %s_%s.root %s_%s_*.root' %
-                      (inname, str(i), inname, str(i)))
+            os.system('rm %s.root' % (str(i)))
+            os.system('hadd %s.root %s_*.root' %
+                      (str(i), str(i)))
     elif(float(sys.argv[1]) in inenergy_list):
         i = float(sys.argv[1])
-        os.system('rm %s_%s.root' % (inname, str(i)))
-        os.system('hadd %s_%s.root %s_%s_*.root' %
-                  (inname, str(i), inname, str(i)))
+        os.system('rm %s.root' % (str(i)))
+        os.system('hadd %s.root %s_*.root' %
+                  (str(i), str(i)))
     else:
         print('能量点错误')
