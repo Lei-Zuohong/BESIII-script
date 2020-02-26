95f0ad0f4b010690d689a51baa177c66228cfdb0
Leizuohong <lzh791397845@gmail.com>
2020-02-26 15:58:57 +0800

    123

--- resub.py
+++ resub.py
@@ -2,10 +2,9 @@ import re
 import os
 
 content = '''
-60726917.0      leizh           01/13 20:24     0+02:04:08      R  0   732.0 xboss /scratchfs/bes/leizh/opp/mc/rec/omeganpt/2.2324_03.txt
-60726918.0      leizh           01/13 20:24     0+02:04:08      R  0   732.0 xboss /scratchfs/bes/leizh/opp/mc/rec/omeganpt/2.0500_00.txt
-60726989.0      leizh           01/13 20:24     0+17:55:40      R  0   732.0 xboss /scratchfs/bes/leizh/opp/mc/rec/omeganpt/2.1500_04.txt
-60727001.0      leizh           01/13 20:24     0+17:55:39      R  0   976.0 xboss /scratchfs/bes/leizh/opp/mc/rec/omeganpt/3.0800_04.txt
+64964425.0      leizh           02/22 08:12     1+01:43:30      R  0   29.0 xboss /scratchfs/bes/leizh/opp/sima/omeganpw/2.9500_05.txt
+64964426.0      leizh           02/22 08:12     1+01:43:30      R  0   26.0 xboss /scratchfs/bes/leizh/opp/sima/omeganpw/2.0000_03.txt
+
 '''
 with open('resub.txt', 'w') as outfile:
     outfile.write(content)
@@ -15,5 +14,5 @@ for i in contents:
     method = r'(.*)      leizh(.*)xboss (.*)'
     check = re.match(method, i)
     if(check):
-        os.system('hep_rm %s'%(check.group(1)))
-        os.system('boss.condor %s'%(check.group(3)))
+        os.system('hep_rm %s' % (check.group(1)))
+        os.system('boss.condor %s' % (check.group(3)))
