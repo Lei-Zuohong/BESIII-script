fec81662a7997666f8d22f2348afb26c919c4a6c
Leizuohong <lzh791397845@gmail.com>
2020-06-10 19:38:06 +0800

    123

--- resub.py
+++ resub.py
@@ -8,12 +8,12 @@ content = '''
 80374440.0      leizh           06/10 13:19     0+06:07:08      R  0   122.0 xboss /scratchfs/bes/leizh/opp/backa/script/2.1250_008.txt
 80374544.0      leizh           06/10 13:20     0+06:05:17      R  0   146.0 xboss /scratchfs/bes/leizh/opp/backa/script/2.9000_003.txt
 80374545.0      leizh           06/10 13:20     0+06:05:14      R  0   146.0 xboss /scratchfs/bes/leizh/opp/backa/script/2.9000_004.txt
-80374608.0      leizh           06/10 13:20     0+06:04:39      R  0   146.0 xboss /scratchfs/bes/leizh/opp/backa/script/3.0800_009.txt'''
-with open('resub.txt', 'w') as outfile:
-    outfile.write(content)
-with open('resub.txt', 'r') as infile:
-    contents = infile.readlines()
-for i in contents:
+80374608.0      leizh           06/10 13:20     0+06:04:39      R  0   146.0 xboss /scratchfs/bes/leizh/opp/backa/script/3.0800_009.txt
+'''
+
+lines = content.splitlines()
+num = len(lines)
+for i in lines:
     method = r'(.*)      leizh(.*)xboss (.*)'
     check = re.match(method, i)
     if(check):
