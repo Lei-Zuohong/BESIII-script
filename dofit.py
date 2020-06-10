32ab0c3e9c50c5eb2c6c25d6410c004b16bdf297
Leizuohong <lzh791397845@gmail.com>
2020-06-10 19:30:17 +0800

    123

--- dofit.py
+++ dofit.py
@@ -10,10 +10,13 @@ import headpy.hbes.hfit as hfit
 tempfolder = sys.argv[1]
 backfunction = sys.argv[2]
 signfunction = sys.argv[3]
+energy = sys.argv[4]
+energy = float(energy)
 histpkl = '%s/temphist.pkl' % (tempfolder)
 optionpkl = '%s/tempoption.pkl' % (tempfolder)
 datasetpkl = '%s/tempdataset.pkl' % (tempfolder)
-hfit.dofit(histpkl,
+hfit.dofit(energy,
+           histpkl,
            optionpkl,
            datasetpkl,
            backfunction=backfunction,
